337a0e1d29e97c0ce2530aeb2227939347f520ba
Eunice Wong <djhopper95@gmail.com>
2018-02-04 01:05:05 -0500

    add ScalaUDF module for dynamically importing Scala UDFs

---
+++ src/main/python/ScalaUDF.py
@@ -0,0 +1,38 @@
+# Archives Unleashed Toolkit (AUT):
+# An open-source platform for analyzing web archives.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyspark.sql.column import Column
+from pyspark.sql.column import _to_java_column
+from pyspark.sql.column import _to_seq
+from pyspark.sql.functions import col
+
+class ScalaUDF:
+    """
+    ScalaUDF is a class to be instantiated when you need to use a Scala UDF
+    in a PySpark job. Calling add(UDFName) will create a method of the
+    class instance that corresponds to the UDF in question, if it exists.
+
+    Note that Scala UDFs must first implement the getUDF() method and
+    an apply() method before they can be imported in this way.
+    """
+
+    def add(self, sc, udf_name):
+        def new_udf(col):
+            udf_pkg = sc._jvm.io.archivesunleashed.spark.matchbox
+            udf = getattr(udf_pkg, udf_name).getUDF()
+            return Column(udf.apply(_to_seq(sc, [col], _to_java_column)))
+        new_udf.__doc__ = "{} is a Scala UDF imported for PySpark".format(udf_name)
+        new_udf.__name__ = udf_name
+        setattr(self, new_udf.__name__, new_udf)
